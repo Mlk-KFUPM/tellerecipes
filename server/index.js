1ae54cfb0d297a95b8a92e2054fde683d2212c5f
abdalmalekl <abdalmalekfs@gmail.com>
2025-12-01 19:45:54 +0300

    updating the index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
 const { app } = require("./server");
+const { connectDB } = require("./db");
 
-module.exports = app;
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
